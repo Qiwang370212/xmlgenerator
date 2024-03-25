70ea96bd8d974757138df2f43d235301c661d92d
Qiwang370212 <116966958+Qiwang370212@users.noreply.github.com>
2024-03-25 17:22:39 +0000

    finished the applicant data part

--- app.py
+++ app.py
@@ -1,39 +1,63 @@
 from flask import Flask, render_template, request
 import xml.etree.ElementTree as ET
-import xml.dom.minidom as minidom
+import pandas as pd
+import os
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def upload_form():
     if request.method == 'POST':
-        first_name = request.form['first_name']
-        last_name = request.form['last_name']
-        append_to_xml(first_name, last_name)
+        append_to_xml(request.form)
         return "Form submitted successfully!"
-    return render_template('XMLGenerator.html')
+    # Call index function to render the template with nationality options
+    return index()
 
-def append_to_xml(first_name, last_name):
+def index():
+    # Define the path to the Excel file
+    excel_file = os.path.join(app.root_path, 'static', 'countrycode.xlsx')
+
+    country_nationality = pd.read_excel(excel_file, sheet_name='Nationality codes')
+    nationality_countries = country_nationality['Nationality'].tolist()
+    nationality_codes = country_nationality['Code'].tolist()
+    nationality_options = zip(nationality_countries, nationality_codes)
+
+    country_birth = pd.read_excel(excel_file, sheet_name='Country codes')
+    birth_countries = country_birth['Nationality'].tolist()
+    birth_codes = country_birth['Code'].tolist()
+    birth_options = zip(birth_countries, birth_codes)
+
+    return render_template('XMLGenerator.html', nationality_options=nationality_options, birth_options=birth_options)
+
+def append_to_xml(form_data):
     tree = ET.parse('static/Testing.xml')
     root = tree.getroot()
 
-    person = ET.SubElement(root, 'person')
-    first = ET.SubElement(person, 'first')
-    first.text = first_name
-    last = ET.SubElement(person, 'last')
-    last.text = last_name
+    CAS = ET.Element('CAS')
+    root.append(CAS)
 
-    # Serialize the XML element tree to a string
-    xml_str = ET.tostring(root, encoding='utf-8')
-    
-    # Parse the XML string to an XML document
-    xml_doc = minidom.parseString(xml_str)
-    
-    # Get the XML as a string without extra newlines
-    pretty_xml_str = xml_doc.toxml(encoding='utf-8')
-    ET.indent(tree, '  ')
-    # Write the XML string to a file
-    tree.write("modifyed.xml", encoding="utf-8", xml_declaration=True)
+    # Append data to ApplicantData
+    ApplicantData = ET.SubElement(CAS, 'ApplicantData')
+    for field in ['ApplicantID', 'FamilyName', 'GivenName', 'Nationality', 'Gender', 'CountryOfBirth', 'DateOfBirth', 'ApplicantPassportOrTravelDocumentNumber']:
+        element = ET.SubElement(ApplicantData, field)
+        # Special handling for DateOfBirth field
+        if field == 'DateOfBirth':  
+            FullDate_element = ET.SubElement(element, 'FullDate')
+            FullDate_element.text = form_data[field]
+        elif field == 'Nationality':
+            element.text = form_data.get(field, '')  # Get the value from the form data
+        else:
+            element.text = form_data[field]
+
+    # Append data to CourseDetails
+    CourseDetails = ET.SubElement(CAS, 'CourseDetails')
+    element = ET.SubElement(CourseDetails, 'CourseCurriculumTitle')
+    element.text = form_data['CourseCurriculumTitle']
+
+    ET.indent(root, '  ')
+
+    tree.write("static/Testing.xml", encoding="utf-8", xml_declaration=True)
+
 
 if __name__ == '__main__':
-    app.run(debug=True, host="127.0.0.1", port="8000")
+    app.run(debug=True, host="127.0.0.1", port="5000")
