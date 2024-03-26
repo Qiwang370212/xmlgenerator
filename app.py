f7074169a2d9ef69db13aa1d17dc8afdd80c9322
Qiwang370212 <qiwang370212@gmail.com>
2024-03-26 14:52:45 +0000

    added nonsponsored institution and course title and level

--- app.py
+++ app.py
@@ -18,55 +18,80 @@ def upload_form():
 def index():
     excel_file = os.path.join(app.root_path, 'static', 'countrycode.xlsx')
 
-    country_nationality = pd.read_excel(excel_file, sheet_name='Nationality codes')
-    nationality_countries = country_nationality['Nationality'].tolist()
-    nationality_codes = country_nationality['Code'].tolist()
-    nationality_options = zip(nationality_countries, nationality_codes)
+    # Read nationality data
+    nationality_data = pd.read_excel(excel_file, sheet_name='Nationality codes')
+    nationality_options = zip(nationality_data['Nationality'].tolist(), nationality_data['Code'].tolist())
 
-    country_birth = pd.read_excel(excel_file, sheet_name='Country codes')
-    birth_countries = country_birth['Nationality'].tolist()
-    birth_codes = country_birth['Code'].tolist()
-    birth_options = zip(birth_countries, birth_codes)
+    # Read birth country data
+    birth_country_data = pd.read_excel(excel_file, sheet_name='Country codes')
+    birth_options = zip(birth_country_data['Nationality'].tolist(), birth_country_data['Code'].tolist())
 
+    # Read course title data
     course_titles = pd.read_excel(excel_file, sheet_name='Course Info')
     course_title_options = course_titles['Course Title'].tolist()
 
-    academic_level = pd.read_excel(excel_file, sheet_name='Course Level')
-    academic_display_text = academic_level['Display text'].tolist()
-    academic_BDT_level = academic_level['BDT Code'].tolist()
-    academic_level_options = zip(academic_display_text, academic_BDT_level)
+    # Read academic level data
+    academic_level_data = pd.read_excel(excel_file, sheet_name='Course Level')
+    academic_level_options = zip(academic_level_data['Display text'].tolist(), academic_level_data['BDT Code'].tolist())
 
-    return render_template('XMLGenerator.html', nationality_options=nationality_options, birth_options=birth_options, course_title_options=course_title_options, academic_level_options=academic_level_options)
+    # Read country data
+    country_data = pd.read_excel(excel_file, sheet_name='Country codes')
+    country_options = zip(country_data['Nationality'].tolist(), country_data['Code'].tolist())
+
+    return render_template('XMLGenerator.html', nationality_options=nationality_options, 
+                           birth_options=birth_options, course_title_options=course_title_options, 
+                           academic_level_options=academic_level_options, country_options=country_options)
+
 
 def append_to_xml(form_data):
-    tree = ET.parse('static/Testing.xml')
-    root = tree.getroot()
+    try:
+        tree = ET.parse('static/Testing.xml')
+        root = tree.getroot()
 
-    CAS = ET.Element('CAS')
-    root.append(CAS)
+        CAS = ET.Element('CAS')
+        root.append(CAS)
 
-    ApplicantData = ET.SubElement(CAS, 'ApplicantData')
-    for field in ['ApplicantID', 'FamilyName', 'GivenName', 'Nationality', 'Gender', 'CountryOfBirth', 'DateOfBirth', 'ApplicantPassportOrTravelDocumentNumber']:
-        element = ET.SubElement(ApplicantData, field)
-        if field == 'DateOfBirth':  
-            FullDate_element = ET.SubElement(element, 'FullDate')
-            FullDate_element.text = form_data[field]
-        elif field == 'GivenName':
-            given_name = form_data.get('FirstName', '') + ' ' + form_data.get('MiddleName', '')
-            element.text = given_name.strip()
+        ApplicantData = ET.SubElement(CAS, 'ApplicantData')
+        for field in ['ApplicantID', 'FamilyName', 'GivenName', 'Nationality', 'Gender', 'CountryOfBirth', 'DateOfBirth', 'ApplicantPassportOrTravelDocumentNumber']:
+            element = ET.SubElement(ApplicantData, field)
+            if field == 'DateOfBirth':  
+                FullDate_element = ET.SubElement(element, 'FullDate')
+                FullDate_element.text = form_data[field]
+            elif field == 'GivenName':
+                given_name = form_data.get('FirstName', '') + ' ' + form_data.get('MiddleName', '')
+                element.text = given_name.strip()
+            else:
+                element.text = form_data[field]
+
+        if form_data.get('PartneredInstitution') == 'yes':
+            NonSponsorEducationInstitutionData = ET.SubElement(CAS, 'NonSponsorEducationInstitutionData')
+            PartnerInstitutionDetails = ET.SubElement(NonSponsorEducationInstitutionData, 'PartnerInstitutionDetails')
+            Name = ET.SubElement(PartnerInstitutionDetails, 'Name')
+            Name.text = form_data.get('PartneredInstitutionName', '')
+            AddressDetails = ET.SubElement(PartnerInstitutionDetails, 'AddressDetails')
+            for field in ['AddressLine', 'City', 'CountyAreaDistrict', 'PostCode', 'Country']:
+                element = ET.SubElement(AddressDetails, field)
+                if field == 'Country':
+                    # Access the country field directly without concatenating
+                    element.text = form_data.get(field, '')
+                else:
+                    element.text = form_data.get('PartneredInstitution' + field, '')
         else:
-            element.text = form_data[field]
+            NonSponsorEducationInstitutionData = ET.SubElement(CAS, 'NonSponsorEducationInstitutionData')
 
-    CourseDetails = ET.SubElement(CAS, 'CourseDetails')
-    element = ET.SubElement(CourseDetails, 'CourseCurriculumTitle')
-    element.text = form_data['CourseCurriculumTitle']
+        CourseDetails = ET.SubElement(CAS, 'CourseDetails')
+        element = ET.SubElement(CourseDetails, 'CourseCurriculumTitle')
+        element.text = form_data.get('CourseCurriculumTitle', '')
 
-    element = ET.SubElement(CourseDetails, 'AcademicLevel')
-    element.text = form_data['AcademicLevel']
+        element = ET.SubElement(CourseDetails, 'AcademicLevel')
+        element.text = form_data.get('AcademicLevel', '')
 
-    ET.indent(root, '  ')
+        ET.indent(root, '  ')
 
-    tree.write("static/Testing.xml", encoding="utf-8", xml_declaration=True)
+        tree.write("static/Testing.xml", encoding="utf-8", xml_declaration=True)
+        return True, "Form submitted successfully!"
+    except Exception as e:
+        return False, f"An error occurred: {str(e)}"
 
 
 if __name__ == '__main__':
